ff61f782e3c59e43b5521272d5ec8792abc7407b
sam ghoreishi <sam20gh@gmail.com>
2019-02-10 23:00:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  dwarf.each do |dwarf|
+  turn_into_frog(student)
+end
 end
 
 def summon_captain_planet# code an argument here
